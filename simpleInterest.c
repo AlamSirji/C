6d5b559e643fa3dd7134166ff0cadda304950d4e
AlamSirji <mail.alamsirji@gmail.com>
2022-09-24 23:41:43 +0530

    cls or clearscr added for both OSs

--- simpleInterest.c
+++ simpleInterest.c
@@ -5,12 +5,15 @@ Date : 24.09.2022
 *************************************************************/
 
 #include<stdio.h>
+#include<stdlib.h>
 void main(){
     float principle, rate, simpleInterest;
     int time;
+    printf("Welcome to Simple Interest Calculator");
+    system("cls||clear");
     principle = 5000; // Rs. 5000
     rate = 10; // 10 %
     time = 5; // 5 years
     simpleInterest = (principle * rate * time) / 100;
-    printf("The Simple Interest for P = %f, R = %f, and T = %d is %f",principle, rate, time, simpleInterest);
+    printf("The Simple Interest is %.2f", simpleInterest);
 }
